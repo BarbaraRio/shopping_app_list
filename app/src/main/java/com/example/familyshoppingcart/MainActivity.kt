6bfe0df593ed9cccfd601f5aed6cbf8da41118cf
Thomas Bruns <thomas.bruns@dock.tech>
2023-04-15 13:53:24 -0300

    fazendo alguns testes

--- app/src/main/java/com/example/familyshoppingcart/MainActivity.kt
+++ app/src/main/java/com/example/familyshoppingcart/MainActivity.kt
@@ -3,48 +3,29 @@ package com.example.familyshoppingcart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import android.widget.Button
+import android.widget.Toast
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.familyshoppingcart.ui.theme.FamilyShoppingCartTheme
-import android.widget.Button
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FamilyShoppingCartTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-        val button = findViewById<Button>(R.id.save_button)
-        button.setOnClickListener {
+        setContentView(R.layout.activity_main);
+
+        val saveButton = findViewById<Button>(R.id.save_button)
+        saveButton.setOnClickListener{
             Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FamilyShoppingCartTheme {
-        Greeting("Android")
-    }
-}
